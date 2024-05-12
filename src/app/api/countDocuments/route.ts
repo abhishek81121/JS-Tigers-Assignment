221b4b943d2bc836a96d63945898f7a48196c48e
Abhishek Kumar Singh <abhishek81121@gmail.com>
2024-05-12 21:54:21 +0530

    corrected the api route

--- src/app/api/countDocuments/route.ts
+++ src/app/api/countDocuments/route.ts
@@ -2,7 +2,7 @@ import { conn } from "@/schema/connection";
 import { vendorModel, vendorSchema } from "@/schema/schemaVendor";
 import mongoose from "mongoose";
 import { NextResponse } from "next/server";
-export async function GET(response: NextResponse) {
+export async function GET(response: Response) {
   await conn();
   const data = await vendorModel.find().skip(0).limit(6);
 
